05c0d0849ad487e2d41d7c0a58569557961ad4c5
bitTal <albafjez@gmail.com>
2017-08-11 12:30:37 +0200

    Remove missplaced code

--- pages/about.js
+++ pages/about.js
@@ -13,26 +13,6 @@ class AboutPage extends Page {
   render() {
     const { url, session } = this.props;
 
-    const getThreshold = (value) => {
-      const threshold = {
-        1: 55,
-        2: 105,
-        3: 200
-      };
-      const status = {
-        2: ['fail', 'best'],
-        3: ['fail', 'medium', 'best'],
-        4: ['fail', 'weak', 'medium', 'best'],
-        5: ['fail', 'bad', 'medium', 'good', 'best']
-      };
-      const values = Object.values(threshold);
-      const len = values.filter(v => value > v).length;
-
-      return status[values.length + 1][len];
-    };
-
-    console.log(getThreshold(56))
-
     return (
       <Layout
         title="About"
