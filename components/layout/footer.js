76da031196c5afc1724db513b2a3ae677a6dcc2d
Miguel Barrenechea <miguel.barrenechea@vizzuality.com>
2017-09-05 13:39:55 +0200

    Footer: implemented

--- components/layout/footer.js
+++ components/layout/footer.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import { Link } from 'routes';
 
+import { FOOTER_LINKS } from 'constants/footer';
 
 export default() => (
   <footer className="c-footer">
-    Footer
+    <h1>
+      <Link route="home">
+        <a>Kenya Dashboard</a>
+      </Link>
+    </h1>
+
+    <ul className="nav-list">
+      {FOOTER_LINKS.map(item =>
+        <li key={item.route} >
+          <Link route={item.route}>
+            <a>{item.label}</a>
+          </Link>
+        </li>
+      )}
+    </ul>
+
   </footer>
 );
---
+++ constants/footer.js
@@ -0,0 +1,7 @@
+const FOOTER_LINKS = [
+  { route: 'home', label: 'Home' },
+  { route: 'dashboard', label: 'Dashboard' },
+  { route: 'about', label: 'About the Alliance' }
+];
+
+export { FOOTER_LINKS };
