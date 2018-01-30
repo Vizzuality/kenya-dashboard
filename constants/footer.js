4ec3d83e2b6e79f667b412e97a12157c5ffc8301
Miguel Barrenechea <miguel.barrenechea@vizzuality.com>
2018-01-30 12:48:07 +0100

    Footer: osileleia logo

--- constants/footer.js
+++ constants/footer.js
@@ -1,7 +1,7 @@
 const FOOTER_LOGOS = [
-  { alt: 'Vizzuality', src: '/static/images/footer/vizzuality.png' },
-  { alt: 'Conservation International', src: '/static/images/footer/conservation-international.png' },
-  { alt: 'Vital Signs', src: '/static/images/footer/vital-signs.png' },
+  { href: 'http://vizzuality.com/', alt: 'Vizzuality', src: '/static/images/footer/vizzuality.png' },
+  { href: 'https://www.conservation.org/Pages/default.aspx', alt: 'Conservation International', src: '/static/images/footer/conservation-international.png' },
+  { href: 'http://vitalsigns.org/', alt: 'Vital Signs', src: '/static/images/footer/vital-signs.png' },
   { alt: 'Osilalei Ltd.', src: '/static/images/footer/osilalei-ltd.png' }
 ];
 
