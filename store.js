4bcf80f0b37b8afc28131176a16a476c025c500e
David Inga <david.inga@vizzuality.com>
2018-06-25 10:57:33 +0200

    removed opbeat

--- store.js
+++ store.js
@@ -1,17 +1,8 @@
-import initOpbeat from 'opbeat-react';
 import React from 'react';
-import { createOpbeatMiddleware } from 'opbeat-react/redux'
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import * as reducers from 'modules';
-
-if (process.env.NODE_ENV === 'production') {
-  initOpbeat({
-    orgId: process.env.OPBEAT_ORG_ID,
-    appId: process.env.OPBEAT_APP_ID
-  });
-}
 
 const reducer = combineReducers({
   ...reducers
@@ -26,7 +17,7 @@ const initStore = (initialState = {}) =>
     composeWithDevTools(
       /* The router middleware MUST be before thunk otherwise the URL changes
       * inside a thunk function won't work properly */
-      applyMiddleware(thunk, createOpbeatMiddleware())
+      applyMiddleware(thunk)
     )
   );
 
